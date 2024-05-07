179ad0c35dd17257584db20cc5d30b78b37e2934
Vishal Bind <137315909+vishaljbind@users.noreply.github.com>
2024-05-07 20:02:03 +0530

    initial commit

---
+++ server/flight-server-css-manifest.js
@@ -0,0 +1,15 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "D:\\project\\ResuMint\\src\\app\\layout.tsx": [
+      "D:\\project\\ResuMint\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "D:\\project\\ResuMint\\src\\app\\page": [
+      "D:\\project\\ResuMint\\src\\app\\globals.css"
+    ],
+    "D:\\project\\ResuMint\\src\\app\\resume-import\\page": [
+      "D:\\project\\ResuMint\\src\\app\\globals.css"
+    ]
+  }
+}
